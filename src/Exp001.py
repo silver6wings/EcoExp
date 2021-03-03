069626630286c6c9a45d3183c4ba7799b46b9410
silver6wings <silver6wings@163.com>
2021-03-03 09:50:03 +0800

    add plt

--- src/Exp001.py
+++ src/Exp001.py
@@ -1,16 +1,19 @@
-from Person import Person
 from random import random
+import matplotlib.pyplot as plt
 
-p_count = 20
+p_count = 100
 init_balance = 100
 
+class Person:
+    def __init__(self, balance):
+        self.balance = balance
 
 if __name__ == '__main__':
     p = []
     for i in range(p_count):
         p.append(Person(init_balance))
 
-    for g in range(1000000):
+    for g in range(50000000):
         p_from = int(random() * p_count)
         p_to = int(random() * p_count)
 
@@ -24,3 +27,5 @@ if __name__ == '__main__':
 
     a.sort()
     print(a)
+    plt.bar(range(len(a)), a)
+    plt.show()
--- src/Person.py
+++
@@ -1,8 +0,0 @@
-
-class Person:
-    def __init__(self, balance):
-        self.balance = balance
-
-if __name__ == '__main__':
-    p = Person()
-    print(p.balance)
